e5f66de12345d09dd4daceacb3136788b678aa69
BTNguyen9280 <briannguyen9280@yahoo.com>
2023-02-09 22:10:26 -0800

    Solve exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  const sorted = people.sort(function(a,b) {
+      var d = new Date().getFullYear()
+      let lastGuy;
+      let nextGuy;
+      if (a.hasOwnProperty('yearOfDeath')) {
+        lastGuy = a.yearOfDeath - a.yearOfBirth}
+      else {
+          lastGuy = d - a.yearOfBirth
+      }
+      if (b.hasOwnProperty('yearOfDeath')){
+        nextGuy = b.yearOfDeath - b.yearOfBirth}
+      else {
+        nextGuy = d - b.yearOfBirth
+      }
+      return lastGuy > nextGuy ? -1 : 1
+  })
+  return sorted[0]
 };
 
 // Do not edit below this line
